66f24f9e97f61a69f43ccb30b27ebd7d86952c54
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-10-23 14:51:35 +0900

    9. String template

---
+++ app/src/main/java/com/example/kotlinstudy/9. String template.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinstudy
+
+fun main(){
+    // 로그 찍을 때 정말 유용함
+    val a = 10
+    val name = "안녕"
+    val isHigh = true
+
+    //간단하고 직관적으로 볼 수 있음
+    println("$a ${name.length} $isHigh")
+}
